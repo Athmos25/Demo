27d1808d09f82e01024601cb4396b7b25bd1a919
Athmos25 <46523552+Athmos25@users.noreply.github.com>
2019-02-12 15:53:33 +0100

    Add files via upload

---
+++ Partiel_Cs_MONIER/Log.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Partiel_Cs_MONIER
+{
+    public class Log
+    {
+        //Propriétés
+        public string Message { get; set; }
+        public DateTime Date  { get; set; }
+        public string Niveau  { get; set; }
+       
+
+        public Log()
+        {
+            //Constructeur vide
+        }
+
+        //Constructeur
+        public Log(string message, string date, string niveau)
+        {
+            Message = message;
+            Date    = date;
+            Niveau  = niveau;
+        }
+    }
+
+    
+
+
+}
---
+++ Partiel_Cs_MONIER/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+
+namespace Partiel_Cs_MONIER
+{
+    class Program
+    {
+        private static void addLog()
+        {
+            //Instanciation d'un objet Log
+            Log log     = new Log();
+            log.Message = "Blabla";
+            //Utilisation du DateTime.Now pour récupérer la date système
+            log.Date    = DateTime.Now;
+            log.Niveau  = "information";
+
+
+            Console.Write("Début de la sérialisation...");
+
+            var serializer = JsonSerializer.Create();
+            using (StreamWriter sw = new StreamWriter(@"d:\test.json"))
+            {
+                using (JsonWriter writer = new JsonTextWriter(sw))
+                {
+                    serializer.Serialize(writer, log);
+                }
+            }
+        }
+
+
+        private static void saveFile()
+        {
+           
+            var json = JsonConvert.SerializeObject(data);
+            File.WriteAllText("test.xml", json);
+
+            //Sérialisation du fichier XML
+            var serialiser = new XmlSerializer(typeof(Log));
+            using (var sw = new StreamWriter("test.xml"))
+            {
+                serialiser.Deserialize(sw, log);
+            }
+        }
+
+
+        private static void showFiles()
+        {
+            if (Date.contains(query, StringComparison)
+            {
+
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            var exit = false;
+
+            while (!exit)
+            {
+                //MENU
+                Console.WriteLine("1: Ajouter log");
+                Console.WriteLine("2: Sauvegarder fichier");
+                Console.WriteLine("3: Afficher les logs du fichier XML");
+                Console.WriteLine("q: Quitter");
+
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        addLog();
+                        break;
+                    case "2":
+                        saveFile();
+                        break;
+                    case "3":
+                        showFiles();
+                        break;
+                    case "q":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("invalid input");
+                        break;
+                }
+            }
+        }
+    }
+}
